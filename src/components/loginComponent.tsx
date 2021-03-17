0c357852c430e88ca8e772e74f0e9c08b88e9a94
bandi501 <saiteja.bandi@syngenta.com>
2021-03-17 16:11:40 +0530

    Create Login page

--- src/components/loginComponent.tsx
+++ src/components/loginComponent.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import {TextField,Link,PrimaryButton } from '@fluentui/react';
 
 
 class LoginComponent extends React.Component {
@@ -7,8 +8,19 @@ class LoginComponent extends React.Component {
 
 
     render() {
-        return (<div>
-            Login
+        return (<div className="loginForm">
+             <div className="logoBlock">
+         <img src='src/logo.png' alt="logo" />
+      </div>
+      <TextField label='User Name' placeholder="User Name"/>
+      <TextField label='Password' type='password' placeholder="Password" />
+      <div className="floatBlock">
+            {/* <Toggle className="loginToggle" label="Remember" inlineLabel /> */}
+            <Link className="floatLeft forgotLink" href="https://developer.microsoft.com/en-us/fluentui#/controls/web/link">
+            Forgot Password ?
+            </Link>
+            <PrimaryButton className="floatRight PrimaryBtn" text="Login" allowDisabledFocus  />
+         </div>
         </div>)
     }
 }
